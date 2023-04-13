a4b01e08b889cc705219efa0ab4f210affda21b3
meni2000 <meni2000@naver.com>
2023-04-13 17:48:53 +0900

    create_reviews_html

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
 # Create your views here.
+
+def index(request):
+    reviews = Review.objects.all()
+    context = {
+        'reviews':reviews,
+    }
+    return render(request, 'reviews/index.html',context)
+
+def detail(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+
+    context = {
+        'review' : review,
+    }
+    return render(request, 'reviews/detail.html',context)
+
+
+def create(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = ReviewForm()
+    context ={
+        'form' : form,
+    }
+    return render(request, 'reviews/create.html', context)
+
+def update(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.method == "POST":
+        pass
+    else:
+        form = ReviewForm(instance=review)
+    context = {
+        'review' : review,
+        'form' : form,
+    }
+    return render(request, 'reviews/update.html', context)
+
+def delete(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    review.delete()
+    return redirect('reviews:index')
